285dd3b2c82255d3694ef230059506bfc005fb4a
Alina Rusu <alex.alina.rusu@gmail.com>
2026-02-10 16:37:59 +0100

    fix imports

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,23 +1,6 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest/presets/default',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.(t|j)sx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-        tsconfig: 'tsconfig.json'
-      }
-    ]
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        module: 'nodenext'
-      }
-    }
-  },
-  extensionsToTreatAsEsm: ['.ts', '.tsx']
+  testMatch: ['**/*.test.ts']
 };
